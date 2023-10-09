0af1268c1afbcd8d722f582e538d2ff0b3603835
Andry Zubov <alex470158@gmail.com>
2023-10-09 22:38:39 +0300

    Planning

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/Header/Header";
 import LoginPage from "./components/LoginPage/LoginPage";
 import ContactsPage from "./components/ContactsPage/ContactsPage"
 import DroneList from "./components/DroneList/DroneList"
-
+import Planning from "./components/Planning/Planning";
 function App() {
   return (
       <Router>
@@ -13,6 +13,7 @@ function App() {
               <Route path="/login" element={<LoginPage />} />
               <Route path="/contacts" element={<ContactsPage/>} />
               <Route path="/dronelist" element={<DroneList />} />
+              <Route path="/planning" element={<Planning/>} />
           </Routes>
       </Router>
   );
