7e5cad7de289b7b57159b49cc598bcdb54282055
Dmytro Asparian <supezbiz@gmail.com>
2023-10-26 16:27:56 +0300

    Added frontend and backend testing of login

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import UserA
+import hashlib
 
-# Create your tests here.
+def hash_password(password):
+    return hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+class CheckUserTestCase(TestCase):
+    
+    def setUp(self):
+        user = UserA(username="testuser", password="password123")
+        user.save()
+    
+    def test_correct_credentials(self):
+        hashed_password = hash_password("password123")
+        response = self.client.post('/api/user-check/', {'username': 'testuser', 'password': hashed_password}, content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {'message': True})
+
+    def test_incorrect_credentials(self):
+        response = self.client.post('/api/user-check/', {'username': 'wronguser', 'password': 'wrongpassword'}, content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {'message': False})
---
+++ frontend/src/pages/LoginPage/LoginPage.test.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import LoginPage from "./LoginPage";
+import { loginUser } from "../../api";
+
+// Mock для функции loginUser
+jest.mock("../../api", () => ({
+  loginUser: jest.fn(),
+}));
+
+describe("LoginPage tests", () => {
+  it("renders login and password input", () => {
+    const { getByPlaceholderText } = render(<LoginPage />);
+    const loginInput = getByPlaceholderText("Логін");
+    const passwordInput = getByPlaceholderText("Пароль");
+
+    expect(loginInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+  });
+
+  it("shows success message on successful login", async () => {
+    loginUser.mockResolvedValueOnce(true);
+
+    const { getByPlaceholderText, getByText } = render(<LoginPage />);
+    const loginInput = getByPlaceholderText("Логін");
+    const passwordInput = getByPlaceholderText("Пароль");
+    const loginButton = getByText("Увійти");
+
+    fireEvent.change(loginInput, { target: { value: "testuser" } });
+    fireEvent.change(passwordInput, { target: { value: "password123" } });
+    fireEvent.click(loginButton);
+
+    await waitFor(() => {
+      expect(getByText("Успішний вхід!")).toBeInTheDocument();
+    });
+  });
+  it("shows failure message on unsuccessful login", async () => {
+    loginUser.mockResolvedValueOnce(false); // имитируем неудачный ответ от API
+
+    const { getByPlaceholderText, getByText } = render(<LoginPage />);
+    const loginInput = getByPlaceholderText("Логін");
+    const passwordInput = getByPlaceholderText("Пароль");
+    const loginButton = getByText("Увійти");
+
+    fireEvent.change(loginInput, { target: { value: "wronguser" } });
+    fireEvent.change(passwordInput, { target: { value: "wrongpassword" } });
+    fireEvent.click(loginButton);
+
+    await waitFor(() => {
+      expect(getByText("Невірний логін або пароль.")).toBeInTheDocument();
+    });
+  });
+});
