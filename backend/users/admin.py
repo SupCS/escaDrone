099c5771a02268bcf53e5d7ddc0106f146f1e9dc
Dmytro Asparian <supezbiz@gmail.com>
2023-10-26 20:09:23 +0300

    Bug fix for similar usernames

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import UserA
-# Register your models here.
 
-admin.site.register(UserA)
+class UserAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        # Перевіряємо чи існує
+        existing_user = UserA.objects.filter(username=obj.username).first()
+        
+        if existing_user and existing_user != obj:
+            # Помилка, якщо вже є
+            raise ValueError("Користувач з таким логіном вже існує")
+        
+        # Зберігаємо об'єкт
+        super().save_model(request, obj, form, change)
+
+admin.site.register(UserA, UserAdmin)
