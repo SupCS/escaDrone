27a50b6d55221fd92c646ac541d64f74497f49d7
Dmytro Asparian <supezbiz@gmail.com>
2023-11-19 09:44:17 +0200

    now login is kept in local storage to not logout after reload

--- frontend/src/AuthContext.js
+++ frontend/src/AuthContext.js
@@ -3,13 +3,15 @@ import React, { createContext, useState } from "react";
 export const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => localStorage.getItem("user"));
 
   const login = (username) => {
+    localStorage.setItem("user", username);
     setUser(username);
   };
 
   const logout = () => {
+    localStorage.removeItem("user");
     setUser(null);
   };
 
