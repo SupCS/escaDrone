81a0ef7855e3bbf1bdbf84f4fa922269d20b29fd
Dmytro Asparian <supezbiz@gmail.com>
2023-11-20 19:43:47 +0200

    login layout changes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "./Header.css";
 import headerLogo from "../../assets/images/headerlogo.png";
 import { AuthContext } from "../../AuthContext";
 import { useNavigate } from "react-router-dom";
+import ProfileIcon from "../../assets/images/ProfileIcon.svg";
 
 function Header() {
   const { user, logout } = useContext(AuthContext);
@@ -11,6 +12,22 @@ function Header() {
     logout();
     navigate("/login");
   };
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+  useEffect(() => {
+    const closeDropdown = (e) => {
+      // Проверяем, является ли e.target иконкой профиля
+      if (!e.target.classList.contains("profileIcon")) {
+        setDropdownOpen(false);
+      }
+    };
+
+    document.body.addEventListener("click", closeDropdown);
+    return () => document.body.removeEventListener("click", closeDropdown);
+  }, []);
+
   return (
     <div className="header">
       <div className="left-links">
@@ -29,10 +46,20 @@ function Header() {
         <a href="/contacts" className="header-link">
           Контакти
         </a>
-      </div>
-      <div>
-        {user && <p>Залогінений як: {user}</p>}
-        <button onClick={handleLogout}>Вийти</button>
+        <div className="profile-container">
+          <img
+            className="profileIcon"
+            src={ProfileIcon}
+            alt="profile"
+            onClick={user ? toggleDropdown : () => navigate("/login")}
+          />
+          {dropdownOpen && user && (
+            <div className="dropdown-menu">
+              <p>{user}</p>
+              <button onClick={handleLogout}>Вийти</button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
